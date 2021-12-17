c245defd6f8b97a66910f81903d7be7164c5b261
2020 <fyodor.kazaryan@bk.ru>
2021-12-17 11:45:30 +0300

    partners section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import HeaderSection from './components/HeaderSection';
 import ExpertiseSection from './components/ExpertiseSection';
+import PartnersSection from './components/PartnersSection';
 
 function App() {
   return (
     <>
       <HeaderSection />
       <ExpertiseSection />
+      <PartnersSection />
     </>
   );
 }
